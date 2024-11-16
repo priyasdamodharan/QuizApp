3eee0806e8ec0c7afdc5b9ba63754e69883556e6
priya damodharan <priyasdamodharan@gmail.com>
2024-11-16 14:33:59 -0800

    fetch quiz questions for quiz added

--- src/main/java/com/project/quiz/app/controller/QuizController.java
+++ src/main/java/com/project/quiz/app/controller/QuizController.java
@@ -1,10 +1,14 @@
 package com.project.quiz.app.controller;
 
+import com.project.quiz.app.model.Question;
+import com.project.quiz.app.model.QuestionWrapper;
 import com.project.quiz.app.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -12,10 +16,19 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
+
+
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
     }
+
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
 
 
 }
--- src/main/java/com/project/quiz/app/service/QuizService.java
+++ src/main/java/com/project/quiz/app/service/QuizService.java
@@ -3,13 +3,16 @@ package com.project.quiz.app.service;
 import com.project.quiz.app.dao.QuestionDao;
 import com.project.quiz.app.dao.QuizDao;
 import com.project.quiz.app.model.Question;
+import com.project.quiz.app.model.QuestionWrapper;
 import com.project.quiz.app.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -32,4 +35,16 @@ public class QuizService {
 
     }
 
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+
+    }
 }
