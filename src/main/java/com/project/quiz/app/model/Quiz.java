33ba95e97bfb2af7132b51e934ff95fc1fdcddc9
priya damodharan <priyasdamodharan@gmail.com>
2024-11-16 14:04:55 -0800

    added comments many to many

--- src/main/java/com/project/quiz/app/model/Quiz.java
+++ src/main/java/com/project/quiz/app/model/Quiz.java
@@ -14,6 +14,18 @@ public class Quiz {
     private Integer id;
     private String title;
 
+
+    //Quiz → Question: A single quiz can have multiple questions.
+    //Question → Quiz: A single question can be associated with multiple quizzes.
+
+    //When we use the @ManyToMany annotation, JPA automatically creates a join table to manage the relationship between the two entities.
+    // This table will have two columns:
+    //One column to store the Quiz IDs (foreign keys).
+    //One column to store the Question IDs (foreign keys).
+    //This table doesn't need to be explicitly created in model, and we don't have to define it in the code;
+    // Hibernate/JPA handles it automatically.
     @ManyToMany
     private List<Question> questions;
+
+
 }
